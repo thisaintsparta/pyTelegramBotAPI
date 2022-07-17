5b3384df5f91ff7b061ad537caf906c4ec2c09ff
thisaintsparta <89745081+thisaintsparta@users.noreply.github.com>
2022-07-17 11:19:08 +0200

    Update webhook_flask_heroku_echo.py

--- examples/webhook_examples/webhook_flask_heroku_echo.py
+++ examples/webhook_examples/webhook_flask_heroku_echo.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 import telebot
 
-TOKEN = '<api_token>'
+TOKEN = os.getenv('API_KEY')
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
@@ -30,7 +30,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    bot.set_webhook(url='https://haas-bot-1.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
